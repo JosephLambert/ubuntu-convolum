e08a4f90ffa76ef3c85560ce1fe6e3557544b104
闫磊 <yanlei@yanleideMacBook-Air.local>
2017-05-16 14:29:48 +0800

    figaro

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
     config.storage = :qiniu
-    config.qiniu_access_key    = ENV['j9OHxnXvOkcoxz0VdJPKUqcTatcvCKRvVxO_eoBG']
-    config.qiniu_secret_key    = ENV['UDJaJzvy9xoOm4dPkcoOLsNsvogpUpDSKXGC3NfB']
-    config.qiniu_bucket        = ENV['jdstore']
-    config.qiniu_bucket_domain = ENV['om4lubav4.bkt.clouddn.com']
+    config.qiniu_access_key    = ENV['qiniu_access_key']
+    config.qiniu_secret_key    = ENV['qiniu_secret_key']
+    config.qiniu_bucket        = ENV['qiniu_bucket']
+    config.qiniu_bucket_domain = ENV['qiniu_bucket_domain']
     config.qiniu_block_size    = 4 * 1024 * 1024
     config.qiniu_protocol      = 'http'
     config.qiniu_up_host       = 'http://up.qiniug.com' # 选择不同的区域时，"up.qiniug.com" 不同
