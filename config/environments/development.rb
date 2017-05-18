71441d2aee2ddadb46327507b7f5cf095b715ca6
闫磊 <yanlei@yanleideMacBook-Air.local>
2017-05-18 09:59:58 +0800

    sendcloud settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
     # Settings specified here will take precedence over those in config/application.rb.
     config.action_mailer.default_url_options = { host: 'localhost:3000' }
-    config.action_mailer.delivery_method = :letter_opener
+    # config.action_mailer.delivery_method = :letter_opener
     # In the development environment your application's code is reloaded on
     # every request. This slows down response time but is perfect for development
     # since you don't have to restart the web server when you make code changes.
@@ -52,4 +52,15 @@ Rails.application.configure do
     # Use an evented file watcher to asynchronously detect changes in source code,
     # routes, locales, etc. This feature depends on the listen gem.
     config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+    config.action_mailer.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
+        address: 'smtpcloud.sohu.com',
+        port: 25,
+        domain: 'heroku.com',
+        authentication: 'login',
+        enable_starttls_auto: true,
+        user_name: ENV['SEND_CLOUD_USER_NAME'],
+        password: ENV['SEND_CLOUD_USER_KEY']
+    }
 end
