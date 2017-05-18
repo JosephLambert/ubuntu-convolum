0c1214cd277182ea84f20dd8511bd0def9ba86e6
闫磊 <yanlei@yanleideMacBook-Air.local>
2017-05-18 15:31:02 +0800

    add category

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -50,6 +50,6 @@ class Admin::PostsController < ApplicationController
     private
 
     def post_params
-        params.require(:post).permit(:title, :description)
+        params.require(:post).permit(:title, :description, :category)
      end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518135054) do
+ActiveRecord::Schema.define(version: 20170518135055) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20170518135054) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "category"
   end
 
   create_table "product_lists", force: :cascade do |t|
