0e158c18f83a72e5ef6637fb9fa80f0806c76fc3
闫磊 <478911359@qq.com>
2017-05-21 16:57:12 +0800

    fix migration bug

--- db/migrate/20170518135056_add_two_columns_to_products.rb
+++ db/migrate/20170518135056_add_two_columns_to_products.rb
@@ -1,6 +1,5 @@
 class AddTwoColumnsToProducts < ActiveRecord::Migration[5.0]
     def change
         add_column :products, :category, :string
-        add_column :products, :attachment, :string
     end
 end
