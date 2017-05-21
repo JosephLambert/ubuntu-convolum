2643469fb847b06dd65a3145abf155bcacb8976b
闫磊 <478911359@qq.com>
2017-05-21 13:11:47 +0800

    add zishiying code

--- app/controllers/contactus/posts_controller.rb
+++ app/controllers/contactus/posts_controller.rb
@@ -11,4 +11,22 @@ class Contactus::PostsController < ApplicationController
             redirect_to root_path
       end
     end
+
+    def create
+        @message = Message.new(message_params)
+
+        if @message.save
+            redirect_to :back
+            flash[:notice] = '您已成功提交问题或者建议'
+        else
+            redirect_to :back
+            flash[:warning] = '您提交问题或者建议失败'
+        end
+    end
+
+    private
+
+    def message_params
+        params.require(:message).permit(:name, :email, :comment)
+    end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -11,4 +11,4 @@
 #
 
 class Message < ApplicationRecord
-end
+ end
