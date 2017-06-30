629a7bea1d103b6e3739ca118449279067609079
闫磊 <478911359@qq.com>
2017-06-30 18:55:21 +0800

    w/product show&index page & controller

--- app/controllers/warehouse/products_controller.rb
+++ app/controllers/warehouse/products_controller.rb
@@ -1,2 +1,14 @@
 class Warehouse::ProductsController < ApplicationController
+    def index
+        @products = Product.order('position ASC').published.recent.paginate(page: params[:page], per_page: 5)
+    end
+
+    def show
+        @product = Product.find(params[:id])
+
+        if @product.is_hidden
+            flash[:warning] = 'This product already archieved'
+            redirect_to root_path
+      end
+    end
 end
