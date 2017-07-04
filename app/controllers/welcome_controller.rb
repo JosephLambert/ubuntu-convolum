275dbb062e4f30ef2295736c9919880a5bd7174b
闫磊 <478911359@qq.com>
2017-07-04 16:55:08 +0800

    welcome intro done

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,5 @@
 class WelcomeController < ApplicationController
     def index
-        @intro1 = Intro.first
-        @intro2 = Intro.second
-          # @intro3 = Intro.find(3)
-          # @intro4 = Intro.find(4)
+        @intro = Intro.first
       end
 end
