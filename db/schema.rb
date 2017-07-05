ac8418531747a0423cf41305d386344c6bd60551
闫磊 <478911359@qq.com>
2017-07-05 17:00:12 +0800

    delete cpad model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170704085527) do
+ActiveRecord::Schema.define(version: 20170705075100) do
 
   create_table "aboutus", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -47,6 +47,13 @@ ActiveRecord::Schema.define(version: 20170704085527) do
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.index ["type"], name: "index_ckeditor_assets_on_type"
+  end
+
+  create_table "cpads", force: :cascade do |t|
+    t.string   "image"
+    t.string   "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "intros", force: :cascade do |t|
