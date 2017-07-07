5a72de22a2f9e421081ffe735786d2a282957187
闫磊 <478911359@qq.com>
2017-07-07 14:34:52 +0800

    delete ad uploader

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,5 @@ class WelcomeController < ApplicationController
         @intro2 = Intro.find(8)
         @intro3 = Intro.find(9)
         @intro4 = Intro.find(10)
-        @ads = Ad.limit(3)
       end
 end
