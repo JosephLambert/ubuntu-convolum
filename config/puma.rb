0196eae90511bd147cf542876873e3dd3e5803dc
闫磊 <478911359@qq.com>
2017-08-03 15:16:12 +0800

    login logup forgetpsw  done

--- config/puma.rb
+++ config/puma.rb
@@ -5,30 +5,33 @@
 # and maximum, this matches the default thread size of Active Record.
 #
 
-# threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
-# threads threads_count, threads_count
+# bendi
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }.to_i
+threads threads_count, threads_count
 
-# port        ENV.fetch("PORT") { 3000 }
+port        ENV.fetch('PORT') { 3000 }
 
-# environment ENV.fetch("RAILS_ENV") { "development" }
-# plugin :tmp_restart
+environment ENV.fetch('RAILS_ENV') { 'development' }
+plugin :tmp_restart
 
-environment 'production'
+# aliyun
 
-workers 1
-threads 1, 5
-
-application_path = '/var/www/convolum'
-
-directory "#{application_path}/current"
-
-pidfile "#{application_path}/shared/tmp/pids/puma.pid"
-state_path "#{application_path}/shared/tmp/sockets/puma.state"
-stdout_redirect "#{application_path}/shared/log/puma.stdout.log", "#{application_path}/shared/log/puma.stderr.log"
-
-bind "unix://#{application_path}/shared/tmp/sockets/puma.sock"
-
-activate_control_app "unix://#{application_path}/shared/tmp/sockets/pumactl.sock"
-
-daemonize true
-preload_app!
+# environment 'production'
+#
+# workers 1
+# threads 1, 5
+#
+# application_path = '/var/www/convolum'
+#
+# directory "#{application_path}/current"
+#
+# pidfile "#{application_path}/shared/tmp/pids/puma.pid"
+# state_path "#{application_path}/shared/tmp/sockets/puma.state"
+# stdout_redirect "#{application_path}/shared/log/puma.stdout.log", "#{application_path}/shared/log/puma.stderr.log"
+#
+# bind "unix://#{application_path}/shared/tmp/sockets/puma.sock"
+#
+# activate_control_app "unix://#{application_path}/shared/tmp/sockets/pumactl.sock"
+#
+# daemonize true
+# preload_app!
