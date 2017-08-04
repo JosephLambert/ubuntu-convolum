628328557c49119c06dcbe541e845e532e5e1519
闫磊 <478911359@qq.com>
2017-08-04 18:24:16 +0800

    item total price helper done

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -2,4 +2,8 @@ module CartsHelper
     def render_cart_total_price(cart)
         cart.total_price
     end
+
+    def render_product_price_count(product, quantity)
+        count = (product.price * quantity).to_s
+  end
 end
