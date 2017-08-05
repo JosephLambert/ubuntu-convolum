0eab60c2524b3efbe6dd1233f2b0e87ad4bd7c43
闫磊 <478911359@qq.com>
2017-08-05 11:12:33 +0800

    order account index and  show page done

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -10,4 +10,21 @@ module OrdersHelper
     def render_order_product_price_count(product_price, quantity)
         (product_price * quantity).to_s
   end
+
+    def render_order_status(order)
+        case order.aasm_state
+        when 'order_placed'
+            '请付款以确认订单'
+        when 'paid'
+            '付款成功，订单准备出货'
+        when 'shipping'
+            '订单出货中'
+        when 'shipped'
+            '商品已送达'
+        when 'order_cancelled'
+            '订单已取消'
+        when 'good_returned'
+            '订单已退货'
+        end
+    end
 end
