3b0d4968f6fca346f7c24a0c504220059ef6e329
闫磊 <478911359@qq.com>
2017-08-05 10:27:21 +0800

    smtp bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
         authentication: 'login',
         enable_starttls_auto: false,
         openssl_verify_mode: 'none',
-        domain: 'sEBYi73NkbaXrvbMIFP9SfD9UH0gxHwO.sendcloud.org'
+        domain: '39.108.4.254',
         user_name: ENV['SEND_CLOUD_USER_NAME'],
         password: ENV['SEND_CLOUD_USER_KEY']
     }
