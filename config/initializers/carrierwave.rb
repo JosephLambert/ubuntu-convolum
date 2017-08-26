e52fa469205036243a972e6337423a8153b864e3
闫磊 <478911359@qq.com>
2017-08-26 17:18:29 +0800

    back

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,7 +14,7 @@ CarrierWave.configure do |config|
     config.aliyun_access_id  = 'LTAIOk90tmyBFj9a'
     config.aliyun_access_key = 'mC3LSXBkqsLpmzB5VINlkqY4I7zH4b'
     # 你需要在 Aliyum OSS 上面提前创建一个 Bucket
-    config.aliyun_bucket     = 'convolu'
+    config.aliyun_bucket     = 'convolum'
     # 是否使用内部连接，true - 使用 Aliyun 主机内部局域网的方式访问  false - 外部网络访问
     config.aliyun_internal   = true
     # 配置存储的地区数据中心，默认: cn-hangzhou
