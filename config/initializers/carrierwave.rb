a92500cc84af5050154b193be7759b7808fff8ec
闫磊 <478911359@qq.com>
2017-08-27 16:52:39 +0800

    a

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -23,5 +23,5 @@ CarrierWave.configure do |config|
     config.qiniu_bucket_domain = 'ovbw4tg1r.bkt.clouddn.com'
     config.qiniu_block_size    = 4 * 1024 * 1024
     config.qiniu_protocol      = 'http'
-    config.qiniu_up_host       = 'http://up.qiniug.com' # 选择不同的区域时，"up.qiniug.com" 不同
+    # config.qiniu_up_host       = 'http://up.qiniug.com' # 选择不同的区域时，"up.qiniug.com" 不同
 end
