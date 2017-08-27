db0263e27c064aaa64cd515729307ce4a4d2a0cb
闫磊 <478911359@qq.com>
2017-08-27 12:39:15 +0800

    test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,7 +10,7 @@ CarrierWave.configure do |config|
     # config.aliyun_area     = "cn-hangzhou"
     # 使用自定义域名，设定此项，carrierwave 返回的 URL 将会用自定义域名
     # 自定义域名请 CNAME 到 you_bucket_name.oss-cn-hangzhou.aliyuncs.com (you_bucket_name 是你的 bucket 的名称)
-    config.aliyun_host       = 'convolum.oss-cn-shenzhen.aliyuncs.com'
+    config.aliyun_host       = 'convolum.oss-cn-qingdao.aliyuncs.com'
     # Bucket 为私有读取请设置 true，默认 false，以便得到的 URL 是能带有 private 空间访问权限的逻辑
     # config.aliyun_private_read = false
 end
